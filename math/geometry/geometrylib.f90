596e298775d5ae7a3603850727e05f6b772ad862
Joshua Fraustro <joshua.fraustro@gmail.com>
2019-03-15 17:43:09 -0400

    added prospective projects
    Work time 32 minutes

---
+++ math/geometry/geometrylib.f90
@@ -0,0 +1,17 @@
+!########################################
+!A Fortran 95 module for helping with geometric functions.
+!Triangles
+!Circles
+!Spheres
+!Squares
+!Cubes
+!Cylinders
+!Vertices
+!Planes
+!area
+!volume
+!surface area
+!transformations
+!scaling
+!normals
+!#########################################
---
+++ math/matrices/matrixlib.f90
@@ -0,0 +1,21 @@
+!########################################
+! A Fortran 95 module for helping with common matrix functions.
+!Intended Features:
+!2d matrix
+!3d matrix
+!identity matrix
+!adjugate
+!adjugated
+!copy
+!determinant
+!identity
+!invert
+!inverted
+!normalize
+!normalized
+!to_3x3
+!to_4x4
+!transpose
+!zero
+!is_orthogonal
+!#########################################
